8dbd255527cb7eec499dd63fe5964dd8cac8630f
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-01-22 11:15:14 +0200

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,92 @@
-//Generic, a type connecting to another type
-// const names:Array<string> = []; // string[]
-// //names[0].split('');
-// const promise: Promise<any> = new Promise((resolve)=>{
-//     setTimeout(()=>{
-//         resolve('This is done!');
-//     }, 2000);
-// });
-
-// promise.then(data => {
-//     data.split('');
-// })
-
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+function Logger(logString: string) {
+    console.log('LOGGER FACTORY');
+    return function(constructor: Function) {
+      console.log(logString);
+      console.log(constructor);
+    };
   }
   
-  const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-  console.log(mergedObj);
-  
-  interface Lengthy {
-    length: number;
-  }
-  
-  function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-    let descriptionText = 'Got no value.';
-    if (element.length === 1) {
-      descriptionText = 'Got 1 element.';
-    } else if (element.length > 1) {
-      descriptionText = 'Got ' + element.length + ' elements.';
-    }
-    return [element, descriptionText];
-  }
-  
-  console.log(countAndDescribe(['Sports', 'Cooking']));
-  
-  function extractAndConvert<T extends object, U extends keyof T>(
-    obj: T,
-    key: U
-  ) {
-    return 'Value: ' + obj[key];
-  }
-  
-  extractAndConvert({ name: 'Max' }, 'name');
-  
-  class DataStorage<T extends string | number | boolean> {
-    private data: T[] = [];
-  
-    addItem(item: T) {
-      this.data.push(item);
-    }
-  
-    removeItem(item: T) {
-      if (this.data.indexOf(item) === -1) {
-        return;
+  function WithTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY');
+    return function(constructor: any) {
+      console.log('Rendering template');
+      const hookEl = document.getElementById(hookId);
+      const p = new constructor();
+      if (hookEl) {
+        hookEl.innerHTML = template;
+        hookEl.querySelector('h1')!.textContent = p.name;
       }
-      this.data.splice(this.data.indexOf(item), 1); // -1
-    }
+    };
+  }
   
-    getItems() {
-      return [...this.data];
+  // @Logger('LOGGING - PERSON')
+  @Logger('LOGGING')
+  @WithTemplate('<h1>My Person Object</h1>', 'app')
+  class Person {
+    name = 'Max';
+  
+    constructor() {
+      console.log('Creating person object...');
     }
   }
   
-  const textStorage = new DataStorage<string>();
-  textStorage.addItem('Max');
-  textStorage.addItem('Manu');
-  textStorage.removeItem('Max');
-  console.log(textStorage.getItems());
+  const pers = new Person();
   
-  const numberStorage = new DataStorage<number>();
+  console.log(pers);
   
-  // const objStorage = new DataStorage<object>();
-  // const maxObj = {name: 'Max'};
-  // objStorage.addItem(maxObj);
-  // objStorage.addItem({name: 'Manu'});
-  // // ...
-  // objStorage.removeItem(maxObj);
-  // console.log(objStorage.getItems());
+  // ---
   
-  interface CourseGoal {
+  function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+  }
+  
+  function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+  }
+  
+  function Log3(
+    target: any,
+    name: string | Symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+  }
+  
+  function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+  }
+  
+  class Product {
+    @Log
     title: string;
-    description: string;
-    completeUntil: Date;
-  }
+    private _price: number;
   
-  function createCourseGoal(
-    title: string,
-    description: string,
-    date: Date
-  ): CourseGoal {
-    let courseGoal: Partial<CourseGoal> = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal as CourseGoal;
-  }
+    @Log2
+    set price(val: number) {
+      if (val > 0) {
+        this._price = val;
+      } else {
+        throw new Error('Invalid price - should be positive!');
+      }
+    }
   
-  const names: Readonly<string[]> = ['Max', 'Anna'];
-  // names.push('Manu');
-  // names.pop();
+    constructor(t: string, p: number) {
+      this.title = t;
+      this._price = p;
+    }
+  
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+      return this._price * (1 + tax);
+    }
+  }
